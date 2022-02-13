642f7c4650d4da9ab45758824fb2c8652177350e
Greensaver <greensaver.ua@gmail.com>
2022-02-13 20:51:33 +0200

    lesson_6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         //lesson_4.Run();
-        lesson_5.Run();
+        //lesson_5.Run();
+        lesson_6.Run();
     }
 
 }
