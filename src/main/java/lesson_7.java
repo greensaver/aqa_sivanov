4012e4425f03154833b90bb7874f22c75cd12a64
Greensaver <greensaver.ua@gmail.com>
2022-02-17 19:56:53 +0200

    как предложила

--- src/main/java/lesson_7.java
+++ src/main/java/lesson_7.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class lesson_7 {
 
@@ -11,8 +9,7 @@ public class lesson_7 {
         _scanner = new Scanner(System.in).useDelimiter("\n");
 
         System.out.println("Привет. Введи номер задачи");
-        if (!_scanner.hasNextInt())
-        {
+        if (!_scanner.hasNextInt()) {
             System.out.println("Спасибо! Вы ввели не то что нужно");
             return;
         }
@@ -48,7 +45,7 @@ public class lesson_7 {
         System.out.println("Введите строку");
         String sc = _scanner.next();
 
-        int last = sc.length()-1;
+        int last = sc.length() - 1;
         char ch = sc.charAt(last);
         System.out.println("Последний символ строки: " + ch);
     }
@@ -60,9 +57,9 @@ public class lesson_7 {
 
         System.out.println(sc.endsWith("!!!"));
 
-        if(sc.endsWith("!!!")){
+        if (sc.endsWith("!!!")) {
             System.out.println("Строка заканчивается на !!!");
-        }else{
+        } else {
             System.out.println("Строка НЕ заканчивается на !!!");
         }
     }
@@ -75,9 +72,9 @@ public class lesson_7 {
 
         System.out.println(sc.startsWith("I like"));
 
-        if(sc.startsWith("I like")){
+        if (sc.startsWith("I like")) {
             System.out.println("Строка начинается на: I like");
-        }else{
+        } else {
             System.out.println("Строка НЕ начинается на: I like");
         }
     }
@@ -89,9 +86,9 @@ public class lesson_7 {
 
         System.out.println(sc.contains("Java"));
 
-        if(sc.contains("Java")){
+        if (sc.contains("Java")) {
             System.out.println("Строка содержит подстроку: Java");
-        }else{
+        } else {
             System.out.println("Строка НЕ содержит подстроку: Java");
         }
     }
@@ -105,9 +102,9 @@ public class lesson_7 {
 
         System.out.println(sc.contains("Java"));
 
-        if(index == -1){
+        if (index == -1) {
             System.out.println("Подстрока \"Java\" не найдена");
-        }else{
+        } else {
             System.out.println("Подстрока \"Java\" найдена в " + index);
         }
     }
@@ -144,8 +141,8 @@ public class lesson_7 {
         String java = "Java";
         int index = sc.indexOf(java);
 
-        int lastIndex=index+java.length();
-        String myStr = sc.substring(0,index)+sc.substring(lastIndex, sc.length());
+        int lastIndex = index + java.length();
+        String myStr = sc.substring(0, index) + sc.substring(lastIndex, sc.length());
 
         System.out.println(myStr);
     }
@@ -163,31 +160,25 @@ public class lesson_7 {
         System.out.printf(result);
     }
 
-    // БЫЛО БОЛЬНО!
-
     private static void Excercise3() {
         System.out.println("Введите слова через пробел");
         String input = _scanner.next();
-         String [] words = input.split(" ");
-         int [] degree= new int[words.length];
+        String[] words = input.split(" ");
+        int degree = Integer.MAX_VALUE;
+        String word="";
         for (int i = 0; i < words.length; i++) {
             int check = 1;
-            for (int k = 0; k < words[i].length()-1; k++) {
-                if(words[i].charAt(k) != words[i].charAt(k+1)){
+            for (int k = 0; k < words[i].length() - 1; k++) {
+                if (words[i].charAt(k) != words[i].charAt(k + 1)) {
                     check++;
                 }
             }
-            degree[i]=check;
-        }
-        int minDegree=Arrays.stream(degree).min().getAsInt();
-        for (int i = 0; i < degree.length; i++) {
-            if (degree[i] == minDegree) {
-                System.out.println(words[i]);
-                break;
+            if (check < degree) {
+                degree = check;
+                word = words[i];
             }
         }
-
+        System.out.println(word);
     }
-
 
 }
